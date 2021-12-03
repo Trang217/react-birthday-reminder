44f1ebba06244df74d5a0580ab7b20640e19c959
Trang Nguyen <nguyenminhtrang9213@gmail.com>
2021-12-03 23:56:28 +0100

    final

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import List from "./components/List";
+import data from "./data";
+
+export default function App() {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  );
+}
